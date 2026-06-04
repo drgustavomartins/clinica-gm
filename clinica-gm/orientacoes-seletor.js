a32a41c084114adb267e8e65dce51c55271a50f4
drgustavomartins <gustavo.m.martins@outlook.com>
2026-06-03 21:44:07 -0300

    fix(orientacoes): remove chave extra que causava SyntaxError no seletor

--- clinica-gm/orientacoes-seletor.js
+++ clinica-gm/orientacoes-seletor.js
@@ -1408,7 +1408,6 @@ function montarSeletor() {
           subgrupoOlhosAberto = false;
         });
         subselecaoHtml += '</div>';
-      }
       html += '<label class="seletor-card" data-cod="' + cod + '">' +
         '<input type="checkbox" value="' + cod + '">' +
         '<span class="check-visual"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg></span>' +
